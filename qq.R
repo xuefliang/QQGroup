57dfffbe5a8c68fff9e8459c0dd8c3c6a445774c
xuefliang <xueflaing@gmail.com>
2014-10-30 11:55:49 +0800

    修改名字

--- qq.R
+++ qq.R
@@ -9,6 +9,7 @@ library(wordcloud)
 library(Rwordseg)
 library(igraph)
 library(scales)
+
 file=paste("qq.txt",sep="")
 file.data<-scan(file,what='',sep='\n',encoding="UTF-8")
 #file.data[1:12]
@@ -37,6 +38,7 @@ for(i in 9:length(file.data))
 }
 data$text<-as.character(data$text)
 data$user.name<-as.character(data$user.name)
+
 #发言时间
 split.datetime<-as.data.frame(matrix(unlist(strsplit(as.character(data$time),' ')),ncol=2,byrow=T))
 split.time<-as.data.frame(matrix(unlist(strsplit(as.character(split.datetime$V2),':')),ncol=3,byrow=T))
@@ -60,29 +62,7 @@ ggplot(result,aes(V1,V2))+geom_line()+theme_bw()+
     ,panel.background = element_blank()
   ) +theme(axis.line = element_line(color = 'black'))+scale_y_continuous(breaks=seq(0,450,50))+labs(x='时间 Time', y='消息数量（条）Number of News')+scale_x_datetime(breaks = "3 hour",labels = date_format("%H:%M"))
 #+xlim(0,23)
-#合并每个用户的所有留言
-user.name<-unique(data$user.name)
-text<-c()
-text.num<-c()
-for(i in 1:length(user.name))
-{
-  text.i<-data$text[which(data$user.name==user.name[i])] #用户i的所有留言向量
-  text.i.num<-length(text.i) #用户i发几次留言
-  for(j in 1:text.i.num)
-  {
-    text[i]<-paste(text[i],text.i[j],sep=" ") #把所有的留言向量合并成一个元素
-  }
-  text.num[i]<-text.i.num
-}
-user.text<-data.frame(user.name=user.name,text=text,text.num=text.num)
-user.text$user.name<-as.character(user.text$user.name)
-user.text$text<-as.character(user.text$text)
-rwordseg<-segmentCN(user.text$text,nature=T)
-rwordseg.n<-rwordseg
-for(i in 1:length(rwordseg))
-{
-  rwordseg.n[[i]]<-rwordseg[[i]] [which(names(rwordseg[[i]])=="n")]  #？
-}
+
 #常用话题词云
 ovid<-Corpus(VectorSource(rwordseg.n))
 inspect(ovid)
@@ -90,7 +70,7 @@ ovid<-tm_map(ovid,FUN=removeWords,c("图片","表情","圖片")) #删除qq系统
 inspect(ovid)
 dtm<-DocumentTermMatrix(ovid)
 #findFreqTerms(dtm, 5)
-#findAssocs(dtm,"卡介苗",0.7)
+#findAssocs(dtm,"流行病",0.5)
 tdm<-TermDocumentMatrix(ovid)
 #qq.matrix<-as.matrix(dtm)
 #qq.freq<-apply(qq.matrix,2,sum)
@@ -103,8 +83,8 @@ data <- data.frame(word=names(v),freq=v)
 pal2 <- brewer.pal(8,"Dark2")
 #png("wordcloud_packages.png", width=1280,height=800)  #词云
 #wc<-wordcloud(data$word,data$freq,min.freq=2,max.words=Inf, random.order=FALSE, rot.per=.45, colors=pal2)
-wc<-wordcloud(data$word,data$freq)
-#用户-词项网络关系图
+wc<-wordcloud(data$word,data$freq,min.freq=2,max.words=Inf, random.order=FALSE, rot.per=.45, colors=pal2)
+
 from<-c()
 to<-c()
 for(i in 1:length(user.text$user.name))
@@ -127,21 +107,6 @@ init.igraph<-function(data,dir=F){
 }
 g.dir<-init.igraph(data.frame(from=from,to=to),T)
 #degree=50
-indexword<-(degree(g.dir,mode="in")>=50)
-indexuser<-(degree(g.dir,mode="out")>=150)
-words<-degree(g.dir,mode='in')[indexword]
-user<-degree(g.dir,mode="out")[indexuser]
-wordname<-V(g.dir)[indexword]$label
-username<-V(g.dir)[indexuser]$label
-label=NA
-label[indexword]<-wordname
-label[indexuser]<-username
-V(g.dir)$size=1
-V(g.dir)[user.index]$color="green"
-V(g.dir)[words.index]$color="red"
-svg("userwords.svg",width=40,height=40)
-plot(g.dir,layout=layout.fruchterman.reingold,vertex.size=1,vertex.label=label,vertex.label.family="GB1")
-dev.off()
 
 #重点词项网络图
 std.degree.words=50 #点的入度大于50，则为重点词条
@@ -176,4 +141,21 @@ V(g.dir)$color="white"
 V(g.dir)[user.index]$color="green"
 svg("user.svg",width=40,height=40)
 plot(g.dir,layout=layout.fruchterman.reingold,vertex.label=label,vertex.label.cex=V(g.dir)$size/3,vertex.color=V(g.dir)$color,vertex.label.family="GB1")
+dev.off()
+
+#用户-词项网络关系图
+indexword<-(degree(g.dir,mode="in")>=50)
+indexuser<-(degree(g.dir,mode="out")>=150)
+words<-degree(g.dir,mode='in')[indexword]
+user<-degree(g.dir,mode="out")[indexuser]
+wordname<-V(g.dir)[indexword]$label
+username<-V(g.dir)[indexuser]$label
+label=NA
+label[indexword]<-wordname
+label[indexuser]<-username
+V(g.dir)$size=1
+V(g.dir)[user.index]$color="green"
+V(g.dir)[words.index]$color="red"
+svg("userwords.svg",width=40,height=40)
+plot(g.dir,layout=layout.fruchterman.reingold,vertex.size=1,vertex.label=label,vertex.label.family="GB1")
 dev.off()
